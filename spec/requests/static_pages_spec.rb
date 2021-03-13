e701bb0c7f496ed19123fb400bb96c08cfc4e8c8
takuya kinoshita <kktak02@gmail.com>
2021-03-13 13:33:19 +0900

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "StaticPages", type: :request do
+  describe "GET /home" do
+    it "returns http success" do
+      get root_path
+      expect(response).to have_http_status(:success)
+    end
+
+  end
+
+  describe "GET /help" do
+    it "returns http success" do
+      get "/static_pages/help"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "Get /about" do
+    it "returns http success" do
+      get "/static_pages/about"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+
+
+
+
+end
