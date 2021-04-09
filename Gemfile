83849ee1e39135c0bd179732ad30c71a7548d4ec
takuya kinoshita <kktak02@gmail.com>
2021-04-10 00:19:00 +0900

    modified ja and set favicon

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.4', require: false
 gem 'bootstrap-sass'
 
+gem 'rails-i18n'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = 'update success'
-      redirect_to users_path
+      redirect_to user_path
     else
       flash[:danger] = 'failed to update'
       render 'edit'
