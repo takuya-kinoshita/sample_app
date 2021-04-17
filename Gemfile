89887a96e05d1e064e0bd6645802ad080c3a385d
takuya kinoshita <kktak02@gmail.com>
2021-04-17 09:38:25 +0900

    setting factory

--- Gemfile
+++ Gemfile
@@ -35,7 +35,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails'
   gem 'sqlite3', '~> 1.4'
-
+  gem 'factory_bot_rails'
 end
 
 group :development do
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,5 @@
 FactoryBot.define do
   factory :user do
-    first_name {"takuya"}
-    last_name {"kinoshita"}
-    email {"kkk@gmail.com"}
-    password {"password"}
-    password_confirmation {"password"}
+    
   end
-
 end
